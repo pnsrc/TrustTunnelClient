beee5575191c3801fa89a8242904c0a673a36cf2
Sergey Fionov <sfionov@adguard.com>
2025-05-14 22:51:15 +0300

    Pull request 435: Use new Docker/Packer images
    Squashed commit of the following:
    
    commit 064f5261b06359b04b66c7892ccba21b54f469c3
    Author: Sergey Fionov <sfionov@adguard.com>
    Date:   Wed May 14 22:25:44 2025 +0300
    
        Oops
    
    commit 563b8882cc13c47f80315311752d7a73948b4f0e
    Author: Sergey Fionov <sfionov@adguard.com>
    Date:   Wed May 14 22:22:55 2025 +0300
    
        Fix macOS vs iOS detection in CMake 4.0
    
    commit 2b4042c2b1ce913a244d0c0c4fbaec16993dd408
    Author: Sergey Fionov <sfionov@adguard.com>
    Date:   Wed May 14 22:15:34 2025 +0300
    
        skipci: Test
    
    commit b45cd5d3111bd16df1d256f4b9ff5fad83faab32
    Author: Sergey Fionov <sfionov@adguard.com>
    Date:   Wed May 14 21:22:18 2025 +0300
    
        Use new Docker/Packer images

--- conanfile.py
+++ conanfile.py
@@ -29,8 +29,8 @@ class VpnLibsConan(ConanFile):
     exports_sources = patch_files
 
     def requirements(self):
-        self.requires("dns-libs/2.5.69@adguard_team/native_libs_common", transitive_headers=True)
-        self.requires("native_libs_common/7.0.18@adguard_team/native_libs_common", force=True, transitive_headers=True)
+        self.requires("dns-libs/2.6.2@adguard_team/native_libs_common", transitive_headers=True)
+        self.requires("native_libs_common/7.0.23@adguard_team/native_libs_common", force=True, transitive_headers=True)
 
         self.requires("brotli/1.1.0", transitive_headers=True)
         self.requires("cxxopts/3.1.1", transitive_headers=True)
