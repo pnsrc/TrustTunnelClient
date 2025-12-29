e790c15a42a48f02908816ec9cd787c5b4439c1c
Ilia Zhirov <i.zhirov@adguard.com>
2025-12-29 11:17:13 +0300

    Pull request 580: Update dns-libs to version 2.8.8 (includes fix of vpn-client build)
    Squashed commit of the following:
    
    commit 991c897aabcf39280be3321ecfad054a359ee496
    Author: Ilia Zhirov <i.zhirov@adguard.com>
    Date:   Mon Dec 29 13:00:34 2025 +0500
    
        Update dns-libs to version 2.8.8 (includes fix of vpn-client build)

--- conanfile.py
+++ conanfile.py
@@ -29,7 +29,7 @@ class VpnLibsConan(ConanFile):
     exports_sources = patch_files
 
     def requirements(self):
-        self.requires("dns-libs/2.7.5@adguard/oss", transitive_headers=True)
+        self.requires("dns-libs/2.8.8@adguard/oss", transitive_headers=True)
         self.requires("native_libs_common/8.0.5@adguard/oss", force=True, transitive_headers=True)
 
         self.requires("brotli/1.1.0", transitive_headers=True)
