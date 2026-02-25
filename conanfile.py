496256d71c277f6a65ca0085258ef4a79896c3ca
Alexey Alenich <pnsrc@ya.ru>
2026-02-25 09:10:51 +0600

    Small fixes for build

--- conanfile.py
+++ conanfile.py
@@ -29,6 +29,7 @@ class VpnLibsConan(ConanFile):
     exports_sources = patch_files
 
     def requirements(self):
+        import os
         self.requires("dns-libs/2.8.26@adguard/oss", transitive_headers=True)
         self.requires("native_libs_common/8.0.15@adguard/oss", transitive_headers=True)
 
@@ -44,9 +45,13 @@ class VpnLibsConan(ConanFile):
         self.requires("tomlplusplus/3.3.0")
         self.requires("zlib/1.3.1", transitive_headers=True)
 
+        disable_http3 = os.getenv("DISABLE_HTTP3", "0").lower() in ("1", "on", "true", "yes")
         if "mips" not in str(self.settings.arch):
-            self.requires("quiche/0.17.1@adguard/oss", transitive_headers=True)
-            self.requires("openssl/boring-2024-09-13@adguard/oss", transitive_headers=True, force=True)
+            if not disable_http3:
+                self.requires("quiche/0.17.1@adguard/oss", transitive_headers=True)
+                self.requires("openssl/boring-2024-09-13@adguard/oss", transitive_headers=True, force=True)
+            else:
+                self.requires("openssl/3.1.5-quic1@adguard/oss", transitive_headers=True, force=True)
         else:
             self.requires("openssl/3.1.5-quic1@adguard/oss", transitive_headers=True, force=True)
 
