cd42e8391054403628db75fcc27c0a738abefaa1
Sergey Fionov <sfionov@adguard.com>
2026-02-25 14:41:06 +0000

    Pull request 632: Update NLC to 8.0.23
    Squashed commit of the following:
    
    commit 70698afd898319be07a6a3bd8cd30c89dddf74db
    Author: Sergey Fionov <sfionov@adguard.com>
    Date:   Wed Feb 25 16:09:55 2026 +0200
    
        Update NLC to 8.0.23

--- conanfile.py
+++ conanfile.py
@@ -29,8 +29,8 @@ class VpnLibsConan(ConanFile):
     exports_sources = patch_files
 
     def requirements(self):
-        self.requires("dns-libs/2.8.26@adguard/oss", transitive_headers=True)
-        self.requires("native_libs_common/8.0.15@adguard/oss", transitive_headers=True)
+        self.requires("dns-libs/2.8.27@adguard/oss", transitive_headers=True)
+        self.requires("native_libs_common/8.0.23@adguard/oss", transitive_headers=True)
 
         self.requires("brotli/1.1.0", transitive_headers=True)
         self.requires("cxxopts/3.1.1", transitive_headers=True)
